0bd81d716e8cc0a1e8e7dbe07743cd05c631c1f4
karinah <karinahalzova@gmail.com>
2021-11-05 13:31:51 +0200

    As a user I can register [Delivery #180207562]

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -44,7 +44,7 @@
 
 <figure>
 	<img alt="Success Kid" src="{successkid}">
-	<figcaption>Sapper test!</figcaption>
+	<figcaption>Sapper test!!</figcaption>
 </figure>
 
 <p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
